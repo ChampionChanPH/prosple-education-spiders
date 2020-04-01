c3bceeafef429289ec4a37be23cfd0e801e15f7b
ChampionChan000 <christian.anasco@prosple.com>
2020-04-01 09:38:24 +0800

    updated uos_spider.py

--- prosple_education_spiders/spiders/uos_spider.py
+++ prosple_education_spiders/spiders/uos_spider.py
@@ -10,6 +10,13 @@ class UosSpiderSpider(scrapy.Spider):
     name = 'uos_spider'
     allowed_domains = ['www.sydney.edu.au', 'sydney.edu.au']
     start_urls = ['https://www.sydney.edu.au/courses/search.html?search-type=course&page=1']
+    http_user = 'b4a56de85d954e9b924ec0e0b7696641'
 
     def parse(self, response):
-        pass
+        return SplashRequest(response.request.url, callback=self.course_parse, args={"wait": 50, "timeout": 60})
+
+    def course_parse(self, response):
+        # courses = response.getall()
+        test = response.xpath("//div[@class='container pageTemplate1']").getall()
+        print(test)
+        print(response)
