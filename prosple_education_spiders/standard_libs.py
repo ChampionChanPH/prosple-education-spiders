1cee9b6e62823ac537648ab99eef5baccfcd45a3
jrbacani <johnel.bacani@gmail.com>
2020-04-09 14:58:43 +0800

    update standard libs import

--- prosple_education_spiders/standard_libs.py
+++ prosple_education_spiders/standard_libs.py
@@ -1,6 +1,7 @@
+import scrapy
 import re
-from ..items import Course
-from ..misc_functions import *
+from .items import Course
+from .misc_functions import *
 from datetime import date
 from time import strptime
 from scrapy_splash import SplashRequest
