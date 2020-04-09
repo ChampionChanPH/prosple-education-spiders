480cf979ad612cda60a91923dbe47f6b59d1e2ee
jrbacani <johnel.bacani@gmail.com>
2020-04-09 14:29:29 +0800

    add taxonomy.py

---
+++ prosple_education_spiders/taxonomy.py
@@ -0,0 +1,17 @@
+degree_types = {
+    "1": "Associate Degree",
+    "2": "Bachelor",
+    "3": "Bachelor (Honours)",
+    "4": "Certificate",
+    "5": "Diploma",
+    "6": "Doctorate (PhD)",
+    "7": "Graduate Certificate",
+    "8": "Graduate Diploma",
+    "9": "High School Certificate",
+    "10": "Juris Doctor",
+    "11": "Masters (Coursework)",
+    "12": "Masters (Research)",
+    "13": "Non-Award",
+    "14": "Professional Certificate",
+}
+
