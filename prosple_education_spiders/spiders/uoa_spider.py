977e52d7a682a33bb36c2ff628ebd35b2865172d
ChampionChan000 <christian.anasco@prosple.com>
2020-04-21 17:02:09 +0800

    updated uoa_spider.py

--- prosple_education_spiders/spiders/uoa_spider.py
+++ prosple_education_spiders/spiders/uoa_spider.py
@@ -184,11 +184,4 @@ class UoaSpiderSpider(scrapy.Spider):
 
         course_item.set_sf_dt(self.degrees, ["and", "with"])
 
-        # if re.search("Honours Degree", course_item["courseName"]):
-        #     course_item["specificStudyField"] = re.findall("(?<=[in|of]\s)(.+)", course_item["specificStudyField"], re.DOTALL)[0]
-        #     study_holder = []
-        #     for item in course_item["rawStudyfield"]:
-        #         study_holder.append(re.findall("(?<=[in|of]\s)(.+)", item, re.DOTALL)[0])
-        #     course_item["rawStudyfield"] = study_holder
-
         yield course_item
