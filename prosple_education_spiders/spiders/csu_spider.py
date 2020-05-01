99a12b110a15971d62f2f1dd7f87a702263dd2b8
ChampionChan000 <christian.anasco@prosple.com>
2020-05-01 14:57:39 +0800

    new spider csu_spider.py

---
+++ prosple_education_spiders/spiders/csu_spider.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+# by Christian Anasco
+
+from ..standard_libs import *
+from ..scratch_file import strip_tags
+
+
+def research_coursework(course_item):
+    if re.search("research", course_item["courseName"], re.I):
+        return "12"
+    else:
+        return "11"
+
+
+def bachelor_honours(course_item):
+    if re.search("honours", course_item["courseName"], re.I):
+        return "3"
+    else:
+        return "2"
+
+
+class CsuSpiderSpider(scrapy.Spider):
+    name = 'csu_spider'
+    allowed_domains = ['study.csu.edu.au']
+    start_urls = ['https://study.csu.edu.au/courses/all']
+    institution = "Charles Sturt University (CSU)"
+    uidPrefix = "AU-CSU-"
+
+    degrees = {
+        "graduate certificate": "7",
+        "graduate diploma": "8",
+        "master": research_coursework,
+        "bachelor": bachelor_honours,
+        "doctor": "6",
+        "certificate": "4",
+        "diploma": "5",
+        "associate degree": "1",
+        "non-award": "13",
+        "no match": "15"
+    }
+
+    months = {
+        "January": "01",
+        "February": "02",
+        "March": "03",
+        "April": "04",
+        "May": "05",
+        "June": "06",
+        "July": "07",
+        "August": "08",
+        "September": "09",
+        "October": "10",
+        "November": "11",
+        "December": "12"
+    }
+
+    campuses = {
+        "Adelaide": "719",
+        "Ultimo": "11788",
+        "Sydney": "723",
+        "Perth": "11784",
+        "Brisbane": "720",
+        "Melbourne": "722"
+    }
+
+    teaching_periods = {
+        "year": 1,
+        "semester": 2,
+        "trimester": 3,
+        "quarter": 4,
+        "month": 12,
+        "week": 52,
+        "day": 365
+    }
+
+    def parse(self, response):
+        courses = response.xpath("//div[@id='all-courses-list']//a/@href").getall()
+
+        for course in courses:
+            yield response.follow(course, callback=self.course_parse)
+
+    def course_parse(self, response):
+        course_item = Course()
+
+        course_item["lastUpdate"] = date.today().strftime("%m/%d/%y")
+        course_item["sourceURL"] = response.request.url
+        course_item["published"] = 1
+        course_item["institution"] = self.institution
+        course_item["internationalApplyURL"] = response.request.url
+        course_item["domesticApplyURL"] = response.request.url
+
+        yield course_item
