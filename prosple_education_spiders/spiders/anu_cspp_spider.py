c349e2c2e54a73869b12d25697c4d48fd5749b35
ChampionChan000 <christian.anasco@prosple.com>
2020-05-14 20:48:24 +0800

    updated anu_cspp_spider.py

--- prosple_education_spiders/spiders/anu_cspp_spider.py
+++ prosple_education_spiders/spiders/anu_cspp_spider.py
@@ -111,8 +111,9 @@ class AnuCsppSpiderSpider(scrapy.Spider):
         if course_name is not None:
             course_item.set_course_name(course_name.strip(), self.uidPrefix)
 
-        overview = response.xpath("//div[@class='introduction']/*").get()
-        if course_name is not None:
+        overview = response.xpath("//div[@class='introduction']/*").getall()
+        if len(overview) > 0:
+            overview = "".join(overview)
             course_item["overview"] = strip_tags(overview, False)
 
         career = response.xpath("//*[preceding-sibling::*[contains(text(), 'Career Options')]]").getall()
