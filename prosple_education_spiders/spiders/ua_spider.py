7da380690ca28c1371ddd7d830753633a997c7c7
jrbacani <johnel.bacani@gmail.com>
2020-06-03 16:56:45 +0800

    finished up ua_spider

--- prosple_education_spiders/spiders/ua_spider.py
+++ prosple_education_spiders/spiders/ua_spider.py
@@ -38,6 +38,17 @@ class UaSpiderSpider(scrapy.Spider):
         "foundation certificate": "4"
     }
 
+    campuses = {
+        "City": "47783",
+        "Epsom": "47784",
+        "Grafton": "47785",
+        "Newmarket": "47786",
+        "Tai Tokerau": "47787",
+        "Leigh Marine": "47788",
+        "South Auckland": "47789"
+    }
+    holder = []
+
     def parse(self, response):
         courses = response.xpath("//ul[preceding-sibling::div/div/h3[contains(text(), 'Programmes')]]/li")
         for li in courses:
@@ -63,9 +74,106 @@ class UaSpiderSpider(scrapy.Spider):
 
         course_item.set_sf_dt(self.degrees, ["and"])
 
+        # Overide canonical group
+        course_item["group"] = 2
+        course_item["canonicalGroup"] = "GradNewZealand"
+
         overview = response.xpath("//div[preceding-sibling::div/h2/text()='Programme overview']/p/text()").getall()
         if overview:
-            course_item["overview"] = "\n".join(overview)
+            course_item["overview"] = cleanspace("\n".join(overview))
             course_item.set_summary(" ".join(overview))
+
+        structure = response.xpath("//div[preceding-sibling::div/h2/text()='Programme structure']/div/div/*").getall()
+        if structure:
+            course_item["courseStructure"] = cleanspace("\n".join(structure))
+
+        fees_dom = response.css(".fees-box .domestic dd::text").get()
+        if fees_dom:
+            fees_dom = re.findall("\$([\d,\.]+)", fees_dom)
+            fees_dom = max([float(x.replace(",", "")) for x in fees_dom])
+            course_item["domesticFeeAnnual"] = fees_dom
+
+        fees_int = response.css(".fees-box .international dd::text").get()
+        if fees_int:
+            fees_int = re.findall("\$([\d,\.]+)", fees_int)
+            course_item["internationalFeeAnnual"] = fees_int
+            course_item["internationalApps"] = 1
+
+        quick_facts = response.css("dl.quick-facts__list")
+        for fact in quick_facts:
+            header = cleanspace(fact.css("dt").get())
+            header = re.findall("svg>\s(.*?)\s</dt>", header)[0]
+            if header == "Duration":
+                duration = fact.css("dd::text").getall()
+                if duration:
+                    for i in duration:
+                        if "Full-time" in i:
+                            min_full = [float(x) for x in re.findall("[\d\.]+", i)]
+                            if len(min_full) > 1:
+                                course_item["durationMinFull"] = min(min_full)
+                                course_item["durationMaxFull"] = max(min_full)
+
+                            elif len(min_full) == 1:
+                                course_item["durationMinFull"] = min_full[0]
+
+                            else:
+                                if "half" in i:
+                                    course_item["durationMinFull"] = 0.5
+                                elif "Varies" not in i:
+                                    course_item.add_flag("duration", "No valid Full-time duration: "+ i)
+
+                            if "year" in i.lower():
+                                course_item["teachingPeriod"] = 1
+
+                            elif "month" in i.lower():
+                                course_item["teachingPeriod"] = 12
+
+                            else:
+                                if "Varies" not in i:
+                                    course_item.add_flag("teachingPeriod", "New teaching period found: "+i)
+
+                        elif "Part-time" in  i:
+                            min_part = re.findall(":\s?(\d+\s\w+)", i)
+                            if min_part:
+                                course_item["durationMinPart"] = min_part[0].split(" ")[0]
+                            else:
+                                if "Varies" not in i:
+                                    course_item.add_flag("duration", "No valid Part-time duration: "+i)
+                        else:
+                            course_item.add_flag("duration", "New duration pattern found: "+i)
+
+            elif header == "Next start date":
+                start = fact.css("dd::text").getall()
+                if start:
+                    start = " ".join(start)
+                    start = convert_months(start.split(" "))
+                    course_item["startMonths"] = "|".join(start)
+
+            elif header == "Available locations":
+                campus = fact.css("dd::text").getall()
+                campus = ", ".join(campus).split(", ")
+                campus = map_convert(self.campuses, campus)
+                mode = []
+                if campus["converted"]:
+                    mode.append("In person")
+                    course_item["campusNID"] = "|".join(campus["converted"])
+
+                for failed in campus["failed"]:
+                    if failed in ["Off-campus", "Auckland Online"] and "Online" not in mode:
+                        mode.append("Online")
+
+                    elif failed not in ["", "Overseas"]:
+                        course_item.add_flag("campusNID", "New campus found: "+failed)
+                course_item["modeOfStudy"] = "|".join(mode)
+
+            elif header == "Programme type":
+                level = fact.css("dd::text").getall()
+                if level:
+                    level = " ".join(level)
+                    if "Undergraduate" in level:
+                        course_item["courseLevel"] = "1"
+                    elif "Postgraduate" in level:
+                        course_item["courseLevel"] = "2"
+
         # if "flag" in course_item:
         yield course_item
