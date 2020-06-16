55f052caa9e77afe4416bb97a94527127568297c
ChampionChan000 <christian.anasco@prosple.com>
2020-06-16 22:09:59 +0800

    updated uoq_spider.py - removed dom and int apply links

--- prosple_education_spiders/spiders/acu_spider.py
+++ prosple_education_spiders/spiders/acu_spider.py
@@ -39,19 +39,19 @@ class AcuSpiderSpider(scrapy.Spider):
     ]
     http_user = 'b4a56de85d954e9b924ec0e0b7696641'
     banned_urls = []
-    courses = []
-    institution = "The University of Queensland (UQ)"
-    uidPrefix = "AU-UOQ-"
+    institution = "Australian Catholic University (ACU)"
+    uidPrefix = "AU-ACU-"
 
     campuses = {
-        "Teaching Hospitals": "715",
-        "Ipswich": "716",
-        "Brisbane": "718",
-        "Pharmacy Aust Cntr Excellence": "717",
-        "Herston": "714",
-        "Gatton": "713",
-        "St Lucia": "711",
-        "External": "712"
+        "Sydney": "509",
+        "Adelaide": "508",
+        "National": "510",
+        "Ballarat": "506",
+        "North Sydney": "504",
+        "Canberra": "505",
+        "Strathfield": "503",
+        "Melbourne": "501",
+        "Brisbane": "502"
     }
 
     degrees = {
@@ -104,12 +104,15 @@ class AcuSpiderSpider(scrapy.Spider):
                 course_item["teachingPeriod"] = self.teaching_periods[item]
 
     def parse(self, response):
-        yield SplashRequest(response.request.url, callback=self.main_parse, args={'wait': 20})
+        yield SplashRequest(response.request.url, callback=self.main_parse, args={'wait': 15})
 
     def main_parse(self, response):
         courses = response.xpath("//section[contains(@class, 'search-results-scholarships')]//input["
                                  "@type='hidden']/@value").getall()
 
+        courses = ["https://courses.acu.edu.au/undergraduate/bachelor_of_business_administration_away_from_base",
+                   "https://courses.acu.edu.au/undergraduate/bachelor_of_commerce",
+                   "https://courses.acu.edu.au/undergraduate/bachelor_of_education_fourth_year_upgrade"]
         for item in courses:
             yield response.follow(item, callback=self.course_parse)
 
@@ -125,6 +128,60 @@ class AcuSpiderSpider(scrapy.Spider):
         course_name = response.xpath("//h1/text()").get()
         if course_name:
             course_item.set_course_name(course_name.strip(), self.uidPrefix)
+
+        overview = response.xpath("//div[@id='course--description--domestic']/*[contains(*/text(), "
+                                  "'Description')]/following-sibling::*[1]/*").getall()
+        if overview:
+            course_item.set_summary(strip_tags(overview[0]))
+            holder = []
+            for item in overview:
+                if not re.search("^<p", item):
+                    break
+                else:
+                    holder.append(item)
+            if holder:
+                course_item["overview"] = strip_tags("".join(holder), False)
+
+        entry = response.xpath("//div[@id='course--requirements--domestic']/*[contains(*/text(), "
+                               "'Essential requirements for admission')]/following-sibling::*[1]/*").getall()
+        if entry:
+            course_item["entryRequirements"] = strip_tags("".join(entry), False)
+
+        career = response.xpath("//div[@id='course--career--domestic']/*[contains(*/text(), "
+                                "'Career paths')]/following-sibling::*[1]/*").getall()
+        if career:
+            course_item["careerPathways"] = strip_tags("".join(career), False)
+
+        location = response.xpath("//dt[contains(text(), 'Location:')]/following-sibling::dd").get()
+        if location:
+            campus_holder = []
+            for campus in self.campuses:
+                if re.search(campus, location, re.I | re.M):
+                    campus_holder.append(self.campuses[campus])
+            if campus_holder:
+                course_item["campusNID"] = "|".join(campus_holder)
+
+        cricos = response.xpath("//dt[contains(text(), 'CRICOS:')]/following-sibling::dd").get()
+        if cricos:
+            cricos = re.findall("\d{6}[0-9a-zA-Z]", cricos, re.M)
+            if cricos:
+                course_item["cricosCode"] = ", ".join(cricos)
+                course_item["internationalApps"] = 1
+                course_item["internationalApplyURL"] = response.request.url
+
+        duration = response.xpath("//dt[contains(text(), 'Duration:')]/following-sibling::dd").get()
+        if duration:
+            duration_full = re.findall("(\d*\.?\d+)(?=\s(year|month|semester|trimester|quarter|week|day))", duration,
+                                       re.I | re.M | re.DOTALL)
+            if duration_full:
+                if len(duration_full) == 1:
+                    course_item["durationMinFull"] = float(duration_full[0][0])
+                    self.get_period(duration_full[0][1].lower(), course_item)
+                if len(duration_full) == 2:
+                    duration_full.sort()
+                    course_item["durationMinFull"] = float(duration_full[0][0])
+                    course_item["durationMaxFull"] = float(duration_full[1][0])
+                    self.get_period(duration_full[1][1].lower(), course_item)
 
         course_item.set_sf_dt(self.degrees, degree_delims=["and", "/"])
 
