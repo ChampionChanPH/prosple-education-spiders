754f926d45df98209feca363d9ec8668c71e0d2a
ChampionChan000 <christian.anasco@prosple.com>
2020-07-05 19:45:50 +0800

    updated mon_spider.py

--- prosple_education_spiders/spiders/mon_spider.py
+++ prosple_education_spiders/spiders/mon_spider.py
@@ -153,12 +153,16 @@ class MonSpiderSpider(scrapy.Spider):
     def parse(self, response):
         boxes = response.xpath("//div[contains(@class, 'search-results')]/div[contains(@class, "
                                "'box-featured__wrapper')]")
-        for item in boxes:
-            url = item.xpath(".//div[contains(@class, 'box-featured__blurb')]//h2[contains(@class, "
-                             "'box-featured__heading')]/a/@href").get()
-            degree = item.xpath(".//div[contains(@class, 'box-featured__blurb')]//span[contains(@class, "
-                                "'box-featured__level')]/text()").get()
-            yield response.follow(url, callback=self.course_parse, meta={'degree': degree})
+        # for item in boxes:
+        #     url = item.xpath(".//div[contains(@class, 'box-featured__blurb')]//h2[contains(@class, "
+        #                      "'box-featured__heading')]/a/@href").get()
+        #     degree = item.xpath(".//div[contains(@class, 'box-featured__blurb')]//span[contains(@class, "
+        #                         "'box-featured__level')]/text()").get()
+        #     yield response.follow(url, callback=self.course_parse, meta={'degree': degree})
+
+        url = "https://www.monash.edu/study/courses/find-a-course/2020/regulation-and-compliance-b6020?domestic=true"
+        degree = "Master degree"
+        yield response.follow(url, callback=self.course_parse, meta={'degree': degree})
 
     def course_parse(self, response):
         course_item = Course()
@@ -184,7 +188,7 @@ class MonSpiderSpider(scrapy.Spider):
             degree = degree.strip()
             degree = degree.split("/")
         if response.meta['degree'] not in ["Short course", "Tailored program", "Single-day program", "Seminar",
-                                           "Enabling course"]:
+                                           "Enabling course"] and len(degree) > 1:
             name = name.split(" and ")
         else:
             name = [name]
