9f5764f50936c989b4d4bca6014fdeba1fc0de97
ChampionChan000 <christian.anasco@prosple.com>
2020-07-23 17:48:54 +0800

    new spider qutonline_spider.py

---
+++ prosple_education_spiders/spiders/qutonline_spider.py
@@ -0,0 +1,206 @@
+# -*- coding: utf-8 -*-
+# by Christian Anasco
+
+from ..standard_libs import *
+from ..scratch_file import strip_tags
+
+
+def research_coursework(course_item):
+    if re.search("research", course_item["courseName"], re.I):
+        return "12"
+    else:
+        return "11"
+
+
+def bachelor_honours(course_item):
+    if re.search("honours", course_item["courseName"], re.I):
+        return "3"
+    else:
+        return "2"
+
+
+def get_total(field_to_use, field_to_update, course_item):
+    if "durationMinFull" in course_item and "teachingPeriod" in course_item:
+        if course_item["teachingPeriod"] == 1:
+            if float(course_item["durationMinFull"]) < 1:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"])
+        if course_item["teachingPeriod"] == 12:
+            if float(course_item["durationMinFull"]) < 12:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                                               / 12
+        if course_item["teachingPeriod"] == 52:
+            if float(course_item["durationMinFull"]) < 52:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                                               / 52
+
+
+class QutonlineSpiderSpider(scrapy.Spider):
+    name = 'qutonline_spider'
+    allowed_domains = ['online.qut.edu.au']
+    start_urls = ['https://online.qut.edu.au/online-courses/']
+    banned_urls = ['']
+    institution = 'Deakin University'
+    uidPrefix = 'AU-QUT-ON-'
+
+    campuses = {
+        "Cloud": "52597",
+        "Warrnambool": "584",
+        "Waurn Ponds": "582",
+        "Waterfront": "581",
+        "Burwood": "579"
+    }
+
+    degrees = {
+        "graduate certificate": "7",
+        "graduate diploma": "8",
+        "master": research_coursework,
+        "bachelor": bachelor_honours,
+        "doctor": "6",
+        "certificate": "4",
+        "foundation certificate": "4",
+        "advanced diploma": "5",
+        "diploma": "5",
+        "associate degree": "1",
+        "non-award": "13",
+        "no match": "15"
+    }
+
+    teaching_periods = {
+        "year": 1,
+        "semester": 2,
+        "trimester": 3,
+        "quarter": 4,
+        "month": 12,
+        "week": 52,
+        "day": 365
+    }
+
+    months = {
+        "Jan": "01",
+        "Feb": "02",
+        "Mar": "03",
+        "Apr": "04",
+        "May": "05",
+        "Jun": "06",
+        "Jul": "07",
+        "Aug": "08",
+        "Sep": "09",
+        "Oct": "10",
+        "Nov": "11",
+        "Dec": "12"
+    }
+
+    def get_period(self, string_to_use, course_item):
+        for item in self.teaching_periods:
+            if re.search(item, string_to_use):
+                course_item["teachingPeriod"] = self.teaching_periods[item]
+
+    def parse(self, response):
+        courses = response.xpath("//div[@class='course-title']/a/@href").getall()
+
+        courses = ['https://online.qut.edu.au/online-courses/it-planning-engineering/master-of-project-management/']
+        for item in courses:
+            yield response.follow(item, callback=self.course_parse)
+
+    def course_parse(self, response):
+        course_item = Course()
+
+        course_item['lastUpdate'] = date.today().strftime("%m/%d/%y")
+        course_item['sourceURL'] = response.request.url
+        course_item['published'] = 1
+        course_item['institution'] = self.institution
+        course_item['domesticApplyURL'] = response.request.url
+
+        course_name = response.xpath("//h1/text()").get()
+        if course_name:
+            course_item.set_course_name(course_name.strip(), self.uidPrefix)
+
+        overview = response.xpath("//div[@id='entry_the_copy']/*").getall()
+        if overview:
+            overview = [x for x in overview if strip_tags(x).strip() != '']
+            if overview:
+                course_item.set_summary(strip_tags(overview[0]))
+                course_item['overview'] = strip_tags(''.join(overview), False)
+
+        entry = response.xpath(
+            "//*[contains(text(), 'Entry criteria')]/following-sibling::div[@class='the_copy']/*").getall()
+        if entry:
+            course_item['entryRequirements'] = strip_tags(''.join(entry), False)
+
+        holder = []
+        if overview:
+            holder.extend(overview)
+        if entry:
+            holder.extend(entry)
+        for item in holder:
+            if not re.search('not available to international students', item):
+                course_item["internationalApps"] = 1
+                course_item["internationalApplyURL"] = response.request.url
+
+        course_item['modeOfStudy'] = 'Online'
+
+        duration = response.xpath("//*[contains(text(), 'Duration')]/following-sibling::*").get()
+        if duration:
+            duration_full = re.findall("(\d*\.?\d+)(?=\s(year|month|semester|trimester|quarter|week|day)s?\sfull.time)",
+                                       duration, re.I | re.M | re.DOTALL)
+            duration_part = re.findall("(\d*\.?\d+)(?=\s(year|month|semester|trimester|quarter|week|day)s?\spart.time)",
+                                       duration, re.I | re.M | re.DOTALL)
+            if not duration_full and duration_part:
+                self.get_period(duration_part[0][1].lower(), course_item)
+            if duration_full:
+                if len(duration_full[0]) == 2:
+                    course_item["durationMinFull"] = float(duration_full[0][0])
+                    self.get_period(duration_full[0][1].lower(), course_item)
+                if len(duration_full[0]) == 3:
+                    course_item["durationMinFull"] = min(float(duration_full[0][0]), float(duration_full[0][1]))
+                    course_item["durationMaxFull"] = max(float(duration_full[0][0]), float(duration_full[0][1]))
+                    self.get_period(duration_full[0][2].lower(), course_item)
+            if duration_part:
+                if self.teaching_periods[duration_part[0][1].lower()] == course_item["teachingPeriod"]:
+                    course_item["durationMinPart"] = float(duration_part[0][0])
+                else:
+                    course_item["durationMinPart"] = float(duration_part[0][0]) * course_item["teachingPeriod"] \
+                                                     / self.teaching_periods[duration_part[0][1].lower()]
+            if "durationMinFull" not in course_item and "durationMinPart" not in course_item:
+                duration_full = re.findall("(\d*\.?\d+)(?=\s(year|month|semester|trimester|quarter|week|day))",
+                                           duration,
+                                           re.I | re.M | re.DOTALL)
+                if duration_full:
+                    if len(duration_full) == 1:
+                        course_item["durationMinFull"] = float(duration_full[0][0])
+                        self.get_period(duration_full[0][1].lower(), course_item)
+                    if len(duration_full) == 2:
+                        course_item["durationMinFull"] = min(float(duration_full[0][0]), float(duration_full[1][0]))
+                        course_item["durationMaxFull"] = max(float(duration_full[0][0]), float(duration_full[1][0]))
+                        self.get_period(duration_full[1][1].lower(), course_item)
+
+        intake = response.xpath("//*[contains(text(), 'Start date')]/following-sibling::*").get()
+        if intake:
+            start_holder = []
+            for item in self.months:
+                if re.search(item, intake, re.M):
+                    start_holder.append(self.months[item])
+            if start_holder:
+                course_item["startMonths"] = "|".join(start_holder)
+
+        career = response.xpath("//div[@id='career_ops']//div[@class='content']/*").getall()
+        if career:
+            course_item['careerPathways'] = strip_tags(''.join(career), False)
+
+        learn = response.xpath("//*[contains(text(), 'What you will learn')]/following-sibling::*//div["
+                               "@class='content']/*").getall()
+        if learn:
+            course_item['whatLearn'] = strip_tags(''.join(learn), False)
+
+        course_item['howToApply'] = '''Apply online by filling out our application form 
+            or speak to a Course Consultant on 1300 703 988.'''
+
+        course_item.set_sf_dt(self.degrees, degree_delims=['and', '/'], type_delims=['of', 'in', 'by'])
+
+        yield course_item
