1084c9f59dfd16f66240e154ffc98b6f2c1bc1d8
ChampionChan000 <christian.anasco@prosple.com>
2020-08-06 10:23:16 +0800

    updated aut_spider.py

--- prosple_education_spiders/spiders/aut_spider.py
+++ prosple_education_spiders/spiders/aut_spider.py
@@ -1,46 +1,147 @@
 # -*- coding: utf-8 -*-
-# by: Johnel Bacani
+# by Christian Anasco
 
 from ..standard_libs import *
+from ..scratch_file import strip_tags
 
-def bachelor(course_item):
-    if "doubleDegree" in course_item:
-        if course_item["doubleDegree"] == 1:
-            index = 1 if "degreeType" in course_item else 0
-            if "honour" in course_item["rawStudyfield"][index]:
-                return "3"
-            else:
-                return "2"
 
-    elif "honour" in course_item["courseName"].lower() or "hons" in course_item["courseName"].lower():
+def research_coursework(course_item):
+    if re.search("research", course_item["courseName"], re.I):
+        return "12"
+    else:
+        return "11"
+
+
+def bachelor_honours(course_item):
+    if re.search("honours", course_item["courseName"], re.I):
         return "3"
-
     else:
         return "2"
 
+
+def get_total(field_to_use, field_to_update, course_item):
+    if "durationMinFull" in course_item and "teachingPeriod" in course_item:
+        if course_item["teachingPeriod"] == 1:
+            if float(course_item["durationMinFull"]) < 1:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"])
+        if course_item["teachingPeriod"] == 12:
+            if float(course_item["durationMinFull"]) < 12:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                                               / 12
+        if course_item["teachingPeriod"] == 52:
+            if float(course_item["durationMinFull"]) < 52:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                                               / 52
+
+
 class AutSpiderSpider(scrapy.Spider):
     name = 'aut_spider'
-    # allowed_domains = ['https://www.aut.ac.nz/s/search.html?query=']
-    start_urls = ['https://www.aut.ac.nz/s/search.html?query=&collection=aut-ac-nz-meta-dev&sitetheme=orange&f.Tabs%7CT=Course&tab=Course&num_ranks=1000&form=simple&&_=1592491274316&start_rank=1']
-    http_user = 'b4a56de85d954e9b924ec0e0b7696641'
-    blacklist_urls = []
-    scraped_urls = []
-    superlist_urls = []
+    allowed_domains = ['www.aut.ac.nz', 'aut.ac.nz']
+    start_urls = ['https://www.aut.ac.nz/s/search.html?search_target=main&query=&collection=aut-ac-nz-meta-dev'
+                  '&sitetheme=red&f.Tabs%7CT=Course&tab=Course&form=simple']
+    institution = 'Auckland University of Technology'
+    uidPrefix = 'NZ-AUT'
 
-    institution = "Auckland University of Technology"
-    uidPrefix = "NZ-AUT-"
-
-    degrees = {
-        # "master": "11",
-        # "bachelor": bachelor,
-        # "postgraduate certificate": "7",
-        # "postgraduate diploma": "8",
-        # "artist diploma": "5",
-        # "foundation certificate": "4"
+    campuses = {
+        "Werribee": "762",
+        "Hawthorn": "761",
+        "Creswick": "759",
+        "Burnley": "758",
+        "Dookie": "760",
+        "Southbank": "756",
+        "Off Campus": "757",
+        "Melbourne": "754",
+        "Parkville": "753"
     }
 
-    def parse(self, response):
-        yield SplashRequest(response.request.url, callback=self.catalog_page, args={'wait': 3})
+    degrees = {
+        "graduate certificate": "7",
+        "graduate diploma": "8",
+        "master": research_coursework,
+        "executive master": research_coursework,
+        "senior executive master": research_coursework,
+        "bachelor": bachelor_honours,
+        "doctor": "6",
+        "doctoral program": "6",
+        "certificate": "4",
+        "specialist certificate": "4",
+        "professional certificate": "14",
+        "certificate i": "4",
+        "certificate ii": "4",
+        "certificate iii": "4",
+        "certificate iv": "4",
+        "advanced diploma": "5",
+        "diploma": "5",
+        "associate degree": "1",
+        "juris doctor": "10",
+        "non-award": "13",
+        "no match": "15"
+    }
 
-    def catalog_page(self, response):
-        courses = response.css("div.search-result")
+    teaching_periods = {
+        "year": 1,
+        "semester": 2,
+        "trimester": 3,
+        "quarter": 4,
+        "month": 12,
+        "week": 52,
+        "day": 365
+    }
+
+    months = {
+        "Jan": "01",
+        "Feb": "02",
+        "Mar": "03",
+        "Apr": "04",
+        "May": "05",
+        "Jun": "06",
+        "Jul": "07",
+        "Aug": "08",
+        "Sep": "09",
+        "Oct": "10",
+        "Nov": "11",
+        "Dec": "12"
+    }
+
+    def get_period(self, string_to_use, course_item):
+        for item in self.teaching_periods:
+            if re.search(item, string_to_use):
+                course_item["teachingPeriod"] = self.teaching_periods[item]
+
+    def parse(self, response):
+        boxes = response.xpath("//ol[@id='fb-results']/li[@class='clearfix courseItem']")
+
+        for item in boxes:
+            url = item.xpath(".//h3/a/@href").get()
+            duration = item.xpath(".//td[contains(text(), 'Duration')]/following-sibling::*").get()
+            location = item.xpath(".//td[contains(text(), 'Campus')]/following-sibling::*").get()
+            if url:
+                yield response.follow(url, callback=self.course_parse,
+                                      meta={'duration': duration, 'location': location})
+
+        next_page = response.xpath("//a[@rel='next']/@href").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+    def course_parse(self, response):
+        course_item = Course()
+
+        course_item['lastUpdate'] = date.today().strftime("%m/%d/%y")
+        course_item['sourceURL'] = response.request.url
+        course_item['published'] = 1
+        course_item['institution'] = self.institution
+
+        course_item['durationMinFull'] = response.meta['duration']
+        course_item['campusNID'] = response.meta['location']
+
+        course_name = response.xpath("//h1/text()").get()
+        if course_name:
+            course_item.set_course_name(course_name.strip(), self.uidPrefix)
+
+        yield course_item
