519cee0e75733d27004e0768e2a1fcbd074a39d3
jrbac <johnel.bacani@prosple.com>
2020-08-11 15:32:13 +0800

    major update to tiwa spider

--- prosple_education_spiders/spiders/tiwa_spider.py
+++ prosple_education_spiders/spiders/tiwa_spider.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
-import scrapy
-import re
-from ..items import Course
-from ..misc_functions import *
-from datetime import date
-from time import strptime
-from scrapy_splash import SplashRequest
 
+from ..standard_libs import *
+
+def bachelor(course_item):
+    if "honour" in course_item["courseName"].lower() or "hons" in course_item["courseName"].lower():
+        return "3"
+
+    else:
+        return "2"
 
 class TiwaSpiderSpider(scrapy.Spider):
     name = 'tiwa_spider'
@@ -46,6 +47,20 @@ class TiwaSpiderSpider(scrapy.Spider):
     }
 
     http_user = 'b4a56de85d954e9b924ec0e0b7696641'
+
+    institution = "TAFE International Western Australia"
+    uidPrefix = "AU-TIWA-"
+
+    degrees = {
+        "master": "11",
+        "bachelor": bachelor,
+        "certificate i": "4",
+        "certificate ii": "4",
+        "certificate iii": "4",
+        "certificate iv": "4",
+        "advanced diploma": "5",
+    }
+
     def parse(self, response):
         categories = response.css("div#MSOZoneCell_WebPartWPQ4 a::attr(href)").extract()
         # print(len(courses))
@@ -58,35 +73,39 @@ class TiwaSpiderSpider(scrapy.Spider):
         courses = response.css("a.view-course-btn::attr(href)").extract()
         # print(len(courses))
         for course in courses:
-            yield SplashRequest(response.urljoin(course), callback=self.course_parse, args={'wait': 25.0}, meta={'url': response.urljoin(course)})
+            yield SplashRequest(response.urljoin(course), callback=self.course_parse, args={'wait': 25}, meta={'url': response.urljoin(course)})
 
     def course_parse(self, response):
         canonical_group = "StudyPerth"
         group_number = 23
-        institution = "TAFE International Western Australia"
-        uidPrefix = "AU-TIWA-"
+
 
         course_item = Course()
         course_item["lastUpdate"] = date.today().strftime("%m/%d/%y")
         course_item["sourceURL"] = response.meta['url']
         course_item["group"] = group_number
         course_item["published"] = 1
-        course_item["institution"] = institution
+        course_item["institution"] = self.institution
         course_item["canonicalGroup"] = canonical_group
         course_item["internationalApplyURL"] = response.meta['url']
         course_item["domesticApplyURL"] = response.meta['url']
 
         raw_course_name = response.css("#main h1::text").extract_first()
-        if len(re.findall("\d",raw_course_name.split(" ")[0])) > 0:
-            course_item["courseName"] = re.sub(raw_course_name.split(" ")[0]+" ","",raw_course_name)
-            # course_item["courseCode"] = raw_course_name.split(" ")[0]
+        if raw_course_name:
+            if len(re.findall("\d",raw_course_name.split(" ")[0])) > 0:
+                course_item.set_course_name(re.sub(raw_course_name.split(" ")[0]+" ","",raw_course_name), self.uidPrefix)
+                # course_item["courseCode"] = raw_course_name.split(" ")[0]
 
+            else:
+                course_item.set_course_name(raw_course_name, self.uidPrefix)
         else:
-            course_item["courseName"] = raw_course_name
+            return
 
-        course_item["uid"] = uidPrefix + course_item["courseName"]
-
-        course_item.set_raw_sf()
+        course_item["uid"] = self.uidPrefix + course_item["courseName"]
+        course_item.set_sf_dt(self.degrees)
+        # StudyPerth override
+        course_item["group"] = 23
+        course_item["canonicalGroup"] = "StudyPerth"
 
         codes = response.css("hgroup h3::text").extract_first()
         # print(codes)
@@ -99,44 +118,52 @@ class TiwaSpiderSpider(scrapy.Spider):
                 print("Missing code")
 
         overview = response.css("article.first p::text").extract()
-        if len(overview) > 0:
+        if overview:
             course_item["overviewSummary"] = overview[0]
             course_item["overview"] = "\n".join(overview)
 
-        course_item["entryRequirements"] = response.css("ul#admission-req").extract_first()
-        course_item["careerPathways"] = response.css('div#ctl00_PlaceHolderMain_ctl00_CourseOutline_CourseCareerOpportunities_CareerOpportunities ul').extract_first()
+        entry = response.css("ul#admission-req").extract_first()
+        if entry:
+            course_item["entryRequirements"] = entry
+
+        pathways = response.css('div#ctl00_PlaceHolderMain_ctl00_CourseOutline_CourseCareerOpportunities_CareerOpportunities ul').extract_first()
+        if pathways:
+            course_item["careerPathways"] = pathways
 
         campuses = unique_list(response.css('span[data-bind*="text: Campus.Location()"]::text').extract())
-        campuses = campus_NID(self.campus_map, campuses)
-        course_item["campusNID"] = "|".join(campuses)
+        if campuses:
+            campuses = map_convert(self.campus_map, campuses)
+            course_item["campusNID"] = "|".join(campuses["converted"])
 
 
         course_data = response.css("table.course-data tr")
         course_item["feesRaw"] = []
         for row in course_data:
             row_td = row.css("td").extract_first()
-            if row.css("th span::text").extract_first() == "Duration:":
-                duration_raw = re.findall("-->(.*?)<!",row_td)[0].split(" ")
-                course_item["durationMinFull"] = float(duration_raw[0])
-                course_item["teachingPeriod"] = get_period(duration_raw[1])
+            row_span = row.css("th span::text").extract_first()
+            if row_td and row_span:
+                if row_span == "Duration:":
+                    duration_raw = re.findall("-->(.*?)<!",row_td)[0].split(" ")
+                    course_item["durationMinFull"] = float(duration_raw[0])
+                    course_item["teachingPeriod"] = get_period(duration_raw[1])
 
-                intakes = cleanspace(row.css("#SelectedIntakes::text").extract_first())
-                intakes = intakes.strip("()").split(" ")
-                intakes = convert_months(intakes)
-                course_item["startMonths"] = "|".join(intakes)
+                    intakes = cleanspace(row.css("#SelectedIntakes::text").extract_first())
+                    intakes = intakes.strip("()").split(" ")
+                    intakes = convert_months(intakes)
+                    course_item["startMonths"] = "|".join(intakes)
 
-            elif "tuition fee" in row.css("th span::text").extract_first().lower():
-                value = re.sub(",","",re.findall("\$([\d,]+)",row_td)[0])
-                period = re.findall("per\s(\w+)",row_td)
-                multiplier = re.findall("\((\d+)\s\w+\)",row_td)
+                elif "tuition fee" in row_span.lower():
+                    value = re.sub(",","",re.findall("\$([\d,]+)",row_td)[0])
+                    period = re.findall("per\s(\w+)",row_td)
+                    multiplier = re.findall("\((\d+)\s\w+\)",row_td)
 
-                if period:
-                    multiplier = int(multiplier[0])
-                    if period[0] == "semester" and multiplier > 1:
-                        course_item["internationalFeeAnnual"] = int(value) * 2
-                    course_item["internationalFeeTotal"] = int(value) * multiplier
-
-        yield course_item
+                    if period:
+                        multiplier = int(multiplier[0])
+                        if period[0] == "semester" and multiplier > 1:
+                            course_item["internationalFeeAnnual"] = int(value) * 2
+                        course_item["internationalFeeTotal"] = int(value) * multiplier
+        if "flag" in course_item:
+            yield course_item
 
 
 
