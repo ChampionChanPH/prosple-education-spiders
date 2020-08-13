2765f59649619b14fc0b34a5954e7cfa5bf9d25e
ChampionChan000 <christian.anasco@prosple.com>
2020-08-13 23:38:24 +0800

    new spider duc_spider.py

---
+++ prosple_education_spiders/spiders/duc_spider.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+# by Christian Anasco
+
+from ..standard_libs import *
+from ..scratch_file import strip_tags
+
+
+def research_coursework(course_item):
+    if re.search("research", course_item["courseName"], re.I):
+        return "12"
+    else:
+        return "11"
+
+
+def bachelor_honours(course_item):
+    if re.search("honours", course_item["courseName"], re.I):
+        return "3"
+    else:
+        return "2"
+
+
+def get_total(field_to_use, field_to_update, course_item):
+    if "durationMinFull" in course_item and "teachingPeriod" in course_item:
+        if course_item["teachingPeriod"] == 1:
+            if float(course_item["durationMinFull"]) < 1:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"])
+        if course_item["teachingPeriod"] == 12:
+            if float(course_item["durationMinFull"]) < 12:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                                               / 12
+        if course_item["teachingPeriod"] == 52:
+            if float(course_item["durationMinFull"]) < 52:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                                               / 52
+
+
+class DucSpiderSpider(scrapy.Spider):
+    name = 'duc_spider'
+    start_urls = ['https://www.ducere.edu.au/courses/']
+    banned_urls = []
+    institution = 'Ducere Global Business School'
+    uidPrefix = 'AU-DUC-'
+
+    campuses = {
+        "Victoria": "35122"
+    }
+
+    degrees = {
+        "graduate certificate": "7",
+        "graduate diploma": "8",
+        "master": research_coursework,
+        "mba": research_coursework,
+        "bachelor": bachelor_honours,
+        "doctor": "6",
+        "certificate": "4",
+        "advanced diploma": "5",
+        "diploma": "5",
+        "associate degree": "1",
+        "juris doctor": "10",
+        "non-award": "13",
+        "no match": "15"
+    }
+
+    teaching_periods = {
+        "year": 1,
+        "semester": 2,
+        "trimester": 3,
+        "quarter": 4,
+        "month": 12,
+        "week": 52,
+        "day": 365
+    }
+
+    months = {
+        "January": "01",
+        "February": "02",
+        "March": "03",
+        "April": "04",
+        "May": "05",
+        "June": "06",
+        "July": "07",
+        "August": "08",
+        "September": "09",
+        "October": "10",
+        "November": "11",
+        "December": "12"
+    }
+
+    def get_period(self, string_to_use, course_item):
+        for item in self.teaching_periods:
+            if re.search(item, string_to_use):
+                course_item["teachingPeriod"] = self.teaching_periods[item]
+
+    def parse(self, response):
+        courses = response.xpath("//div[@class='products']/div[contains(@class, 'wtrContainerColor')]")
+
+        for item in courses:
+            url = item.xpath(".//div[contains(@class, 'program-name')]/a/@href").get()
+            duration = item.xpath(".//div[contains(*//text(), 'Duration')]").get()
+            study = item.xpath(".//div[contains(*//text(), 'Study Mode')]").get()
+            if url:
+                yield response.follow(url, callback=self.course_parse, meta={'duration': duration, 'study': study})
+
+    def course_parse(self, response):
+        course_item = Course()
+
+        course_item['lastUpdate'] = date.today().strftime("%m/%d/%y")
+        course_item['sourceURL'] = response.request.url
+        course_item['published'] = 1
+        course_item['institution'] = self.institution
+
+        course_name = response.xpath("//div[contains(@class, 'dhvc_woo_product-meta-field-course_title')]/text()").get()
+        if course_name:
+            if re.search("MBA", course_name):
+                name = re.sub("\(", "- ", course_name)
+                name = re.sub("\)", "", name)
+                course_item.set_course_name(name.strip(), self.uidPrefix)
+            else:
+                course_item.set_course_name(course_name.strip(), self.uidPrefix)
+
+        course_item['durationRaw'] = response.meta['duration']
+        course_item['modeOfStudy'] = response.meta['study']
+
+        overview = response.xpath("//div[@class='vc_empty_space  desktoponly']/following-sibling::*").getall()
+        holder = []
+        for item in overview:
+            if not re.search("^<p", item):
+                break
+            elif re.search("Course Learning Outcomes", item):
+                break
+            else:
+                holder.append(item)
+        if holder:
+            course_item['overview'] = strip_tags(''.join(overview), False)
+
+        start = response.xpath("//div[contains(*//text(), 'Intake Dates')]").getall()
+        start_holder = []
+        if start:
+            start = ''.join(start)
+            for month in self.months:
+                if re.search(month, start, re.M):
+                    start_holder.append(self.months[month])
+        if start_holder:
+            course_item['startMonths'] = '|'.join(start_holder)
+
+        learn = response.xpath("//*[contains(text(), 'You will') or contains(strong/text(), 'You will') or contains("
+                               "strong/text(), 'Course Learning Outcomes')]/following-sibling::*[self::ul or "
+                               "self::ol]").get()
+        if learn:
+            course_item['overview'] = strip_tags(learn, False)
+
+        course_item.set_sf_dt(self.degrees, degree_delims=['and', '/'], type_delims=['of', 'in', 'by', '-'])
+
+        course_item.set_course_name(course_name.strip(), self.uidPrefix)
+
+        yield course_item
+
+
+
+
