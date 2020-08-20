4b835433220a68d081e7d559a8aeed1df5feb98d
ChampionChan000 <christian.anasco@prosple.com>
2020-08-20 13:23:19 +0800

    updated gri_spider.py

---
+++ prosple_education_spiders/spiders/gri_spider.py
@@ -0,0 +1,134 @@
+# -*- coding: utf-8 -*-
+# by Christian Anasco
+
+from ..standard_libs import *
+from ..scratch_file import strip_tags
+
+
+def research_coursework(course_item):
+    if re.search("research", course_item["courseName"], re.I):
+        return "12"
+    else:
+        return "11"
+
+
+def bachelor_honours(course_item):
+    if re.search("honours", course_item["courseName"], re.I):
+        return "3"
+    else:
+        return "2"
+
+
+def get_total(field_to_use, field_to_update, course_item):
+    if "durationMinFull" in course_item and "teachingPeriod" in course_item:
+        if course_item["teachingPeriod"] == 1:
+            if float(course_item["durationMinFull"]) < 1:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"])
+        if course_item["teachingPeriod"] == 12:
+            if float(course_item["durationMinFull"]) < 12:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                                               / 12
+        if course_item["teachingPeriod"] == 52:
+            if float(course_item["durationMinFull"]) < 52:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                                               / 52
+
+
+class GriSpiderSpider(scrapy.Spider):
+    name = 'gri_spider'
+    start_urls = ['https://www.griffith.edu.au/study/degrees?term=&studentType=domestic&studentType=international'
+                  '&academicTermYear=2020']
+    http_user = 'b4a56de85d954e9b924ec0e0b7696641'
+    institution = "Swinburne University of Technology"
+    uidPrefix = "AU-SWI-"
+
+    degrees = {
+        "graduate certificate": "7",
+        "graduate diploma": "8",
+        "master": research_coursework,
+        "bachelor": bachelor_honours,
+        "doctor": "6",
+        "certificate": "4",
+        "victorian certificate": "4",
+        "certificate i": "4",
+        "certificate ii": "4",
+        "certificate iii": "4",
+        "certificate iv": "4",
+        "advanced diploma": "5",
+        "diploma": "5",
+        "associate degree": "1",
+        "non-award": "13",
+        "no match": "15"
+    }
+
+    months = {
+        "January": "01",
+        "February": "02",
+        "March": "03",
+        "April": "04",
+        "May": "05",
+        "June": "06",
+        "July": "07",
+        "August": "08",
+        "September": "09",
+        "October": "10",
+        "November": "11",
+        "December": "12"
+    }
+
+    campuses = {
+        "Hobart": "710",
+        "Croydon": "708",
+        "Off-Campus": "709",
+        "Wantirna": "707",
+        "Richmond Football Club": "706",
+        "Melbourne": "704",
+        "Hawthorn": "703"
+    }
+
+    teaching_periods = {
+        "year": 1,
+        "semester": 2,
+        "trimester": 3,
+        "quarter": 4,
+        "month": 12,
+        "week": 52,
+        "day": 365
+    }
+
+    def get_period(self, string_to_use, course_item):
+        for item in self.teaching_periods:
+            if re.search(item, string_to_use):
+                course_item["teachingPeriod"] = self.teaching_periods[item]
+
+    def parse(self, response):
+        yield SplashRequest(response.request.url, callback=self.sub_parse, args={'wait': 20})
+
+    def sub_parse(self, response):
+        courses = response.xpath("//div[@class='tr']//a/@href").getall()
+
+        print(courses)
+
+        for item in courses:
+            yield SplashRequest(response.urljoin(item), callback=self.course_parse,
+                                args={'wait': 20}, meta={'url': response.urljoin(item)})
+
+    def course_parse(self, response):
+        course_item = Course()
+
+        course_item["lastUpdate"] = date.today().strftime("%m/%d/%y")
+        course_item["sourceURL"] = response.meta['url']
+        course_item["published"] = 1
+        course_item["institution"] = self.institution
+
+        course_name = response.xpath("//h1/text()").get()
+        if course_name:
+            course_item.set_course_name(course_name.strip(), self.uidPrefix)
+
+        yield course_item
