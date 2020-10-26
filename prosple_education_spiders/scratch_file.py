9893da1d5e152fd8b054f28cb23c8cd6642e7328
ChampionChan000 <christian.anasco@prosple.com>
2020-10-26 13:36:27 +0800

    updated scratch_file.py - add parameter to remove hyperlinks

--- prosple_education_spiders/scratch_file.py
+++ prosple_education_spiders/scratch_file.py
@@ -2,11 +2,12 @@ import re
 from lxml.html.clean import clean_html
 
 
-def strip_tags(phrase, remove_all_tags=True):
+def strip_tags(phrase, remove_all_tags=True, remove_hyperlinks=False):
     """
     :param phrase: string to clean
     :param remove_all_tags: if True, remove all HTML tags. if False, h1, h2, h3,
     and h4 tags will be converted to strong tag. Other tags will also be cleaned.
+    :param remove_hyperlinks: if True, will remove all of the hyperlinks <a> on that field.
     :return: cleaned phrase
     """
 
@@ -22,6 +23,9 @@ def strip_tags(phrase, remove_all_tags=True):
         phrase = re.sub("<span.*?>", "<div>", phrase, re.DOTALL)
         phrase = re.sub("</span.*?>", "</div>", phrase, re.DOTALL)
         phrase = re.sub("<img.*?>", "<p>", phrase, re.DOTALL)
+        if remove_hyperlinks:
+            phrase = re.sub("<a.*?>", "", phrase, re.DOTALL)
+            phrase = re.sub("</a.*?>", "", phrase, re.DOTALL)
         if phrase:
             phrase = clean_html(phrase)
         return phrase.strip()
