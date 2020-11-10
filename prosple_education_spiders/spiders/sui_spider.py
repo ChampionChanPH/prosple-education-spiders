c0d62d1a239e1f546a3c63b8e86cd93e1830a002
ChampionChan000 <christian.anasco@prosple.com>
2020-11-10 13:24:10 +0800

    updated sui_spider.py

--- prosple_education_spiders/spiders/sui_spider.py
+++ prosple_education_spiders/spiders/sui_spider.py
@@ -60,7 +60,7 @@ class SuiSpiderSpider(scrapy.Spider):
         "advanced diploma": "5",
         "diploma": "5",
         "associate degree": "1",
-        "vcal in victorian certificate": "9",
+        "vcal victorian certificate": "9",
         "vcal in": "9",
         "non-award": "13",
         "no match": "15"
@@ -142,6 +142,102 @@ class SuiSpiderSpider(scrapy.Spider):
             if course_name:
                 course_item.set_course_name(course_name[0].strip(), self.uidPrefix)
 
+        overview = response.xpath(
+            "//*[@class='s-course-details-page__content']/*[text()='Overview']/following-sibling::*").get()
+        if overview:
+            course_item.set_summary(strip_tags(overview))
+            course_item["overview"] = strip_tags(overview, remove_all_tags=False)
+
+        career = response.xpath(
+            "//*[@class='s-course-details-page__content']/*[text()='Careers']/following-sibling::*").get()
+        if career:
+            course_item['careerPathways'] = strip_tags(career, remove_all_tags=False)
+
+        entry = response.xpath(
+            "//*[@class='s-course-details-page__content']/*[text()='Entry Requirements']/following-sibling::*").get()
+        if entry:
+            course_item['entryRequirements'] = strip_tags(entry, remove_all_tags=False)
+
+        duration = response.xpath("//*[@class='s-course-details-page__content']/*[text()='Expected Time to "
+                                  "Complete']/following-sibling::*").get()
+        if duration:
+            duration_full = re.findall(
+                "(\d*\.?\d+)(?=\s(year|month|semester|trimester|quarter|week|day)s?,?\s+?full)",
+                duration, re.I | re.M | re.DOTALL)
+            duration_part = re.findall(
+                "(\d*\.?\d+)(?=\s(year|month|semester|trimester|quarter|week|day)s?,?\s+?part)",
+                duration, re.I | re.M | re.DOTALL)
+            if not duration_full and duration_part:
+                self.get_period(duration_part[0][1].lower(), course_item)
+            if duration_full:
+                course_item["durationMinFull"] = float(duration_full[0][0])
+                self.get_period(duration_full[0][1].lower(), course_item)
+            if duration_part:
+                if self.teaching_periods[duration_part[0][1].lower()] == course_item["teachingPeriod"]:
+                    course_item["durationMinPart"] = float(duration_part[0][0])
+                else:
+                    course_item["durationMinPart"] = float(duration_part[0][0]) * course_item["teachingPeriod"] \
+                                                     / self.teaching_periods[duration_part[0][1].lower()]
+            if "durationMinFull" not in course_item and "durationMinPart" not in course_item:
+                duration_full = re.findall("(\d*\.?\d+)(?=\s(year|month|semester|trimester|quarter|week|day))",
+                                           duration, re.I | re.M | re.DOTALL)
+                if duration_full:
+                    course_item["durationMinFull"] = float(duration_full[0][0])
+                    self.get_period(duration_full[0][1].lower(), course_item)
+                    # if len(duration_full) == 1:
+                    #     course_item["durationMinFull"] = float(duration_full[0][0])
+                    #     self.get_period(duration_full[0][1].lower(), course_item)
+                    # if len(duration_full) == 2:
+                    #     course_item["durationMinFull"] = min(float(duration_full[0][0]), float(duration_full[1][0]))
+                    #     course_item["durationMaxFull"] = max(float(duration_full[0][0]), float(duration_full[1][0]))
+                    #     self.get_period(duration_full[1][1].lower(), course_item)
+
+        cricos = response.xpath(
+            "//*[contains(@class, 's-course-details-page__badge')][contains(text(), 'Cricos Code')]/text()").get()
+        if cricos:
+            cricos = re.findall("\d{6}[0-9a-zA-Z]", cricos, re.M)
+            if cricos:
+                course_item["cricosCode"] = ", ".join(cricos)
+                course_item["internationalApps"] = 1
+
+        dom_fee = response.xpath("//td[text()='Self-Funded']/following-sibling::*").get()
+        if dom_fee:
+            dom_fee = re.findall("\$(\d*),?(\d+)(\.\d\d)?", dom_fee, re.M)
+            dom_fee = [float(''.join(x)) for x in dom_fee]
+            if dom_fee:
+                course_item["domesticFeeTotal"] = max(dom_fee)
+                # get_total("domesticFeeAnnual", "domesticFeeTotal", course_item)
+
+        csp_fee = response.xpath("//td[text()='Government Subsidised']/following-sibling::*").get()
+        if csp_fee:
+            csp_fee = re.findall("\$(\d*),?(\d+)(\.\d\d)?", csp_fee, re.M)
+            csp_fee = [float(''.join(x)) for x in csp_fee]
+            if csp_fee:
+                course_item["domesticSubFeeTotal"] = max(csp_fee)
+                # get_total("domesticSubFeeAnnual", "domesticSubFeeTotal", course_item)
+
+        location = response.xpath("//*[@id='intake-body']//td[2]/text()").getall()
+        if location:
+            course_item['campusNID'] = '|'.join(location)
+
         course_item.set_sf_dt(self.degrees, degree_delims=["and", "/"], type_delims=["of", "in", "by"])
+
+        international_link = response.xpath(
+            "//a[@class='s-course-details-page__sidebar-item'][contains(text(), 'International Students')]/@href").get()
+        if international_link:
+            yield response.follow(international_link, callback=self.int_parse, meta={'item': course_item})
+        else:
+            yield course_item
+
+    def int_parse(self, response):
+        course_item = response.meta['item']
+
+        int_fee = response.xpath("//td[text()='Total Fees (AUD)']/following-sibling::*").get()
+        if int_fee:
+            int_fee = re.findall("\$(\d*),?(\d+)(\.\d\d)?", int_fee, re.M)
+            int_fee = [float(''.join(x)) for x in int_fee]
+            if int_fee:
+                course_item["internationalFeeTotal"] = max(int_fee)
+                # get_total("internationalFeeAnnual", "internationalFeeTotal", course_item)
 
         yield course_item
