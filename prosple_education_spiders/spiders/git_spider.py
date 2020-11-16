137aac92dfb65a488abf00dd0e72af7712703642
ChampionChan000 <christian.anasco@prosple.com>
2020-11-16 22:27:51 +0800

    new spider git_spider.py

---
+++ prosple_education_spiders/spiders/git_spider.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+# by Christian Anasco
+
+from ..standard_libs import *
+from ..scratch_file import *
+
+
+def research_coursework(course_item):
+    if re.search("research", course_item["courseName"], re.I):
+        return "12"
+    else:
+        return "11"
+
+
+def bachelor_honours(course_item):
+    if re.search("honours", course_item["courseName"], re.I):
+        return "3"
+    else:
+        return "2"
+
+
+def get_total(field_to_use, field_to_update, course_item):
+    if "durationMinFull" in course_item and "teachingPeriod" in course_item:
+        if course_item["teachingPeriod"] == 1:
+            if float(course_item["durationMinFull"]) < 1:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"])
+        if course_item["teachingPeriod"] == 12:
+            if float(course_item["durationMinFull"]) < 12:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                                               / 12
+        if course_item["teachingPeriod"] == 52:
+            if float(course_item["durationMinFull"]) < 52:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                                               / 52
+
+
+class GitSpiderSpider(scrapy.Spider):
+    name = 'git_spider'
+    allowed_domains = ['a']
+    start_urls = [
+        'https://www.thegordon.edu.au/courses/international-courses',
+        'https://www.thegordon.edu.au/courses/all-courses'
+    ]
+
+    international_courses = []
+
+    institution = "Gordon Institute of TAFE"
+    uidPrefix = "AU-GIT-"
+
+    degrees = {
+        "graduate certificate": "7",
+        "graduate diploma": "8",
+        "master": research_coursework,
+        "bachelor": bachelor_honours,
+        "doctor": "6",
+        "certificate": "4",
+        "certificate i": "4",
+        "certificate ii": "4",
+        "certificate iii": "4",
+        "certificate iv": "4",
+        "advanced diploma": "5",
+        "diploma": "5",
+        "associate degree": "1",
+        "vcal victorian certificate": "9",
+        "vcal in": "9",
+        "non-award": "13",
+        "no match": "15"
+    }
+
+    months = {
+        "Jan": "01",
+        "Feb": "02",
+        "Mar": "03",
+        "Apr": "04",
+        "May": "05",
+        "Jun": "06",
+        "Jul": "07",
+        "Aug": "08",
+        "Sep": "09",
+        "Oct": "10",
+        "Nov": "11",
+        "Dec": "12"
+    }
+
+    campuses = {
+        "Mildura": "58005",
+        "Swan Hill": "58006",
+    }
+
+    teaching_periods = {
+        "year": 1,
+        "semester": 2,
+        "trimester": 3,
+        "quarter": 4,
+        "month": 12,
+        "week": 52,
+        "day": 365
+    }
+
+    def get_period(self, string_to_use, course_item):
+        for item in self.teaching_periods:
+            if re.search(item, string_to_use):
+                course_item["teachingPeriod"] = self.teaching_periods[item]
+
+    def parse(self, response):
+        if response.request.url == self.start_urls[0]:
+            self.international_courses.extend(response.xpath("//*[@id='pageCourseSearchDiv']//a/text()").getall())
+        else:
+            courses = response.xpath("//*[@id='pageCourseSearchDiv']//a/text()").getall()
+
+            for item in courses:
+                yield response.follow(item, callback=self.course_parse)
+
+    def course_parse(self, response):
+        course_item = Course()
+
+        course_item['lastUpdate'] = date.today().strftime("%m/%d/%y")
+        course_item['sourceURL'] = response.request.url
+        course_item['published'] = 1
+        course_item['institution'] = self.institution
+
+        course_name = response.xpath("//h1/text()").get()
+        if course_name:
+            course_item.set_course_name(course_name.strip(), self.uidPrefix)
+
+        course_item.set_sf_dt(self.degrees, degree_delims=["and", "/"], type_delims=["of", "in", "by"])
+
+        if course_name in self.international_courses:
+            international_link = response.urljoin(response.request.url, '?i=1')
+            yield response.follow(international_link, callback=self.int_parse, meta={'item': course_item})
+        else:
+            yield course_item
+
+    def int_parse(self, response):
+        course_item = response.meta['item']
+
+        cricos = response.xpath("//*[text()='Cricos Code']/following-sibling::*/text()").get()
+        if cricos:
+            cricos = re.findall("\d{6}[0-9a-zA-Z]", cricos, re.M)
+            if cricos:
+                course_item["cricosCode"] = ", ".join(set(cricos))
+
+        course_item["internationalApps"] = 1
+
+        yield course_item
