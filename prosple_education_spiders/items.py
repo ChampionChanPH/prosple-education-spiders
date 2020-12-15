c9a50fdcd40ea9dd47297f0d1a164977bd7afce9
ChampionChan000 <christian.anasco@prosple.com>
2020-12-15 22:36:39 +0800

    updated items.py - updated set_summary function, current function results to None if first sentence is more than 300 characters. instead, fixed to cut it to 300 characters and add ellipsis (...)

--- prosple_education_spiders/items.py
+++ prosple_education_spiders/items.py
@@ -397,8 +397,14 @@ class Course(scrapy.Item):
         if len(text) > 1:
             temp_holder = []
             char_count = 0
-            for item in text:
-                if char_count + len(item) < max_characters:
+            for index, item in text:
+                if index == 0 and len(item) > max_characters:
+                    cut_summary = text[0][:max_characters + 1]
+                    last_space = cut_summary.rindex(' ')
+                    summary = cut_summary[:last_space] + '...'
+                    temp_holder.append(summary)
+                    break
+                elif char_count + len(item) < max_characters:
                     char_count += len(item) + 1
                     temp_holder.append(item)
             summary = ' '.join(temp_holder).strip()
