18f89080ce2c10b9d0c1fc81a2c34f0536d6d058
ChampionChan000 <christian.anasco@prosple.com>
2020-12-21 21:44:49 +0800

    updated ltu_spider.py - added domestic apply url

--- prosple_education_spiders/spiders/ltu_spider.py
+++ prosple_education_spiders/spiders/ltu_spider.py
@@ -297,6 +297,7 @@ class LtuSpiderSpider(scrapy.Spider):
         course_item['sourceURL'] = response.meta['url']
         course_item['published'] = 1
         course_item['institution'] = self.institution
+        course_item['domesticApplyURL'] = response.meta['url']
 
         course_name = response.xpath("//h1/text()").get()
         if course_name:
