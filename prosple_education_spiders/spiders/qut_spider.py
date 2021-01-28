c9e0954115452229c8f1d24910cd299bebcabae9
jrbac <johnel.bacani@prosple.com>
2021-01-28 13:17:40 +0800

    add utm tags to apply url

--- prosple_education_spiders/spiders/qut_spider.py
+++ prosple_education_spiders/spiders/qut_spider.py
@@ -141,7 +141,7 @@ class QutSpiderSpider(scrapy.Spider):
         course_item["sourceURL"] = response.request.url
         course_item["published"] = 1
         course_item["institution"] = self.institution
-        course_item["domesticApplyURL"] = response.request.url
+        course_item["domesticApplyURL"] = response.request.url+r"?utm_source=referral_partner&utm_medium=prosple&utm_campaign=prosple_course_listing&utm_content=&utm_term=&spad=prosple"
 
         course_name = response.xpath("//h1/span/text()").get()
         if not course_name:
