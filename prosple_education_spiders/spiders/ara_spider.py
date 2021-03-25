f65d031c87db6066c2a503194780c8f0880d24e6
ChampionChan000 <christian.anasco@prosple.com>
2021-03-25 20:48:43 +0800

    new spider ara_spider.py

---
+++ prosple_education_spiders/spiders/ara_spider.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+# by Christian Anasco
+
+from ..standard_libs import *
+from ..scratch_file import *
+
+
+def research_coursework(course_item):
+    if re.search("research", course_item["courseName"], re.I):
+        return "12"
+    else:
+        return "11"
+
+
+def bachelor_honours(course_item):
+    if re.search("honours", course_item["courseName"], re.I):
+        return "3"
+    else:
+        return "2"
+
+
+def get_total(field_to_use, field_to_update, course_item):
+    if "durationMinFull" in course_item and "teachingPeriod" in course_item:
+        if course_item["teachingPeriod"] == 1:
+            if float(course_item["durationMinFull"]) < 1:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"])
+        if course_item["teachingPeriod"] == 12:
+            if float(course_item["durationMinFull"]) < 12:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                                               / 12
+        if course_item["teachingPeriod"] == 52:
+            if float(course_item["durationMinFull"]) < 52:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                                               / 52
+
+
+class AraSpiderSpider(scrapy.Spider):
+    name = 'ara_spider'
+    start_urls = ['https://www.ara.ac.nz/course-search-page']
+    http_user = 'b4a56de85d954e9b924ec0e0b7696641'
+    institution = "Australian Catholic University (ACU)"
+    uidPrefix = "AU-ACU-"
+
+    campuses = {
+        "Sydney": "509",
+        "Adelaide": "508",
+        "National": "510",
+        "Ballarat": "506",
+        "North Sydney": "504",
+        "Canberra": "505",
+        "Strathfield": "503",
+        "Melbourne": "501",
+        "Brisbane": "502"
+    }
+
+    degrees = {
+        "graduate certificate": "7",
+        "graduate diploma": "8",
+        "master": research_coursework,
+        "executive master": research_coursework,
+        "bachelor": bachelor_honours,
+        "doctor": "6",
+        "certificate": "4",
+        "certificate i": "4",
+        "certificate ii": "4",
+        "certificate iii": "4",
+        "certificate iv": "4",
+        "undergraduate certificate": "4",
+        "advanced diploma": "5",
+        "diploma": "5",
+        "associate degree": "1",
+        "non-award": "13",
+        "no match": "15"
+    }
+
+    teaching_periods = {
+        "year": 1,
+        "semester": 2,
+        "trimester": 3,
+        "quarter": 4,
+        "month": 12,
+        "week": 52,
+        "day": 365
+    }
+
+    months = {
+        "January": "01",
+        "February": "02",
+        "March": "03",
+        "April": "04",
+        "May": "05",
+        "June": "06",
+        "July": "07",
+        "August": "08",
+        "September": "09",
+        "October": "10",
+        "November": "11",
+        "December": "12"
+    }
+
+    lua = """
+        function main(splash, args)
+          assert(splash:go(args.url))
+          assert(splash:wait(2.0))
+          local category = splash:select(args.selector_category)
+          assert(category:mouse_click())
+          assert(splash:wait(2.0))
+          return splash:html()
+        end
+    """
+
+    def get_period(self, string_to_use, course_item):
+        for item in self.teaching_periods:
+            if re.search(item, string_to_use):
+                course_item["teachingPeriod"] = self.teaching_periods[item]
+
+    def parse(self, response):
+        yield SplashRequest(response.request.url, callback=self.category_parse, args={'wait': 20})
+
+        yield SplashRequest(response.request.url, callback=self.parse, endpoint='execute',
+                            args={'lua_source': self.lua, 'url': response.request.url,
+                                  'selector_category': 'li.paginationjs-page'})
+
+    def category_parse(self, response):
+        courses = response.xpath("//a[@data-blk='CourseSearchResultItemBlock']/@href").getall()
+
+        for item in courses:
+            yield response.follow(item, callback=self.course_parse)
+
+    def course_parse(self, response):
+        course_item = Course()
+
+        course_item["lastUpdate"] = date.today().strftime("%m/%d/%y")
+        course_item["sourceURL"] = response.request.url
+        course_item["published"] = 1
+        course_item["institution"] = self.institution
+
+        yield course_item
