4445e19841f4c7afd2994011e92b2bf60f77bd01
ChampionChan000 <christian.anasco@prosple.com>
2021-03-29 21:41:02 +0800

    updated nom_spider.py, nor_spider.py, sor_spider.py

--- prosple_education_spiders/spiders/nom_spider.py
+++ prosple_education_spiders/spiders/nom_spider.py
@@ -254,6 +254,10 @@ class NomSpiderSpider(scrapy.Spider):
 
         course_item.set_sf_dt(self.degrees, degree_delims=['and', '/'], type_delims=['of', 'in', 'by', 'for'])
 
+        if 'doubleDegree' in course_item:
+            del course_item['doubleDegree']
+            course_item['rawStudyfield'] = [re.sub('.+ (in|of) ', '', course_item['courseName'], re.DOTALL)]
+
         course_item['group'] = 141
         course_item['canonicalGroup'] = 'CareerStarter'
 
