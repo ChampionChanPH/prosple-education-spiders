b1286db64a0dd8abc998dd250ccfd5f89b1fbee2
ChampionChan000 <christian.anasco@prosple.com>
2021-04-22 13:27:23 +0800

    updated ara_spider.py

--- prosple_education_spiders/spiders/ara_spider.py
+++ prosple_education_spiders/spiders/ara_spider.py
@@ -15,7 +15,7 @@ payload = {
     'x-api-key': api_key,
 }
 
-courses = requests.get(TEST_URL + 'programme', params=payload)
+courses = requests.get(MAIN_URL + 'programme', params=payload)
 courses = courses.json()
 
 
@@ -124,104 +124,104 @@ class AraSpiderSpider(scrapy.Spider):
                 course_item["teachingPeriod"] = self.teaching_periods[item]
 
     def parse(self, response):
-        # for item in courses:
-        course = requests.get(TEST_URL + 'programme/' + 'G21114-STRU', params=payload)
-        course = course.json()
+        for item in courses:
+            course = requests.get(MAIN_URL + 'programme/' + item, params=payload)
+            course = course.json()
 
-        course_item = Course()
+            course_item = Course()
 
-        course_item["lastUpdate"] = date.today().strftime("%m/%d/%y")
+            course_item["lastUpdate"] = date.today().strftime("%m/%d/%y")
 
-        if 'url' in course and course['url']:
-            course_item["sourceURL"] = course['url']
+            if 'url' in course and course['url']:
+                course_item["sourceURL"] = course['url']
 
-        course_item["published"] = 1
-        course_item["institution"] = self.institution
+            course_item["published"] = 1
+            course_item["institution"] = self.institution
 
-        name = None
-        if 'productTitle' in course and course['productTitle']:
-            course_name = course['productTitle']
-            name = course_name[:]
-            if re.search('\(.*(master|bachelor|diploma)', course_name, flags=re.I | re.M | re.DOTALL):
-                course_name = re.sub('\(.*(master|bachelor|diploma).*', '', course_name,
-                                     flags=re.I | re.M | re.DOTALL)
-            elif re.search('computer networking', course_name, flags=re.I) and \
-                    re.search('certificate', course_name, flags=re.I):
-                course_name = re.sub('( instructors)? certificate', '', course_name, flags=re.I)
-            if course_name:
-                course_item['courseName'] = course_name.strip()
+            name = None
+            if 'productTitle' in course and course['productTitle']:
+                course_name = course['productTitle']
+                name = course_name[:]
+                if re.search('\(.*(master|bachelor|diploma)', course_name, flags=re.I | re.M | re.DOTALL):
+                    course_name = re.sub('\(.*(master|bachelor|diploma).*', '', course_name,
+                                         flags=re.I | re.M | re.DOTALL)
+                elif re.search('computer networking', course_name, flags=re.I) and \
+                        re.search('certificate', course_name, flags=re.I):
+                    course_name = re.sub('( instructors)? certificate', '', course_name, flags=re.I)
+                if course_name:
+                    course_item['courseName'] = course_name.strip()
 
-        if 'longDescription' in course and course['longDescription']:
-            course_item['overview'] = strip_tags(course['longDescription'], remove_all_tags=False,
-                                                 remove_hyperlinks=True)
+            if 'longDescription' in course and course['longDescription']:
+                course_item['overview'] = strip_tags(course['longDescription'], remove_all_tags=False,
+                                                     remove_hyperlinks=True)
 
-        if 'shortDescription' in course and course['shortDescription']:
-            course_item.set_summary(strip_tags(course['shortDescription']))
-        elif 'longDescription' in course and course['longDescription']:
-            course_item.set_summary(strip_tags(course['longDescription']))
+            if 'shortDescription' in course and course['shortDescription']:
+                course_item.set_summary(strip_tags(course['shortDescription']))
+            elif 'longDescription' in course and course['longDescription']:
+                course_item.set_summary(strip_tags(course['longDescription']))
 
-        if 'uiCode' in course and course['uiCode']:
-            course_item['courseCode'] = course['uiCode']
+            if 'uiCode' in course and course['uiCode']:
+                course_item['courseCode'] = course['uiCode']
 
-        if 'outcome' in course and course['outcome']:
-            course_item['careerPathways'] = strip_tags(course['outcome'], remove_all_tags=False,
-                                                       remove_hyperlinks=True)
+            if 'outcome' in course and course['outcome']:
+                course_item['careerPathways'] = strip_tags(course['outcome'], remove_all_tags=False,
+                                                           remove_hyperlinks=True)
 
-        if 'duration' in course and course['duration']:
-            duration_full = re.findall(
-                "(\d*\.?\d+)(?=\s(year|month|semester|trimester|quarter|week|day)s?\s+?full)",
-                course['duration'], re.I | re.M | re.DOTALL)
-            duration_part = re.findall(
-                "(\d*\.?\d+)(?=\s(year|month|semester|trimester|quarter|week|day)s?\s+?part)",
-                course['duration'], re.I | re.M | re.DOTALL)
-            if not duration_full and duration_part:
-                self.get_period(duration_part[0][1].lower(), course_item)
-            if duration_full:
-                course_item["durationMinFull"] = float(duration_full[0][0])
-                self.get_period(duration_full[0][1].lower(), course_item)
-            if duration_part:
-                if self.teaching_periods[duration_part[0][1].lower()] == course_item["teachingPeriod"]:
-                    course_item["durationMinPart"] = float(duration_part[0][0])
-                else:
-                    course_item["durationMinPart"] = float(duration_part[0][0]) * course_item["teachingPeriod"] \
-                                                     / self.teaching_periods[duration_part[0][1].lower()]
-            if "durationMinFull" not in course_item and "durationMinPart" not in course_item:
-                duration_full = re.findall("(\d*\.?\d+)(?=\s(year|month|semester|trimester|quarter|week|day))",
-                                           course['duration'], re.I | re.M | re.DOTALL)
+            if 'duration' in course and course['duration']:
+                duration_full = re.findall(
+                    "(\d*\.?\d+)(?=\s(year|month|semester|trimester|quarter|week|day)s?\s+?full)",
+                    course['duration'], re.I | re.M | re.DOTALL)
+                duration_part = re.findall(
+                    "(\d*\.?\d+)(?=\s(year|month|semester|trimester|quarter|week|day)s?\s+?part)",
+                    course['duration'], re.I | re.M | re.DOTALL)
+                if not duration_full and duration_part:
+                    self.get_period(duration_part[0][1].lower(), course_item)
                 if duration_full:
                     course_item["durationMinFull"] = float(duration_full[0][0])
                     self.get_period(duration_full[0][1].lower(), course_item)
+                if duration_part:
+                    if self.teaching_periods[duration_part[0][1].lower()] == course_item["teachingPeriod"]:
+                        course_item["durationMinPart"] = float(duration_part[0][0])
+                    else:
+                        course_item["durationMinPart"] = float(duration_part[0][0]) * course_item["teachingPeriod"] \
+                                                         / self.teaching_periods[duration_part[0][1].lower()]
+                if "durationMinFull" not in course_item and "durationMinPart" not in course_item:
+                    duration_full = re.findall("(\d*\.?\d+)(?=\s(year|month|semester|trimester|quarter|week|day))",
+                                               course['duration'], re.I | re.M | re.DOTALL)
+                    if duration_full:
+                        course_item["durationMinFull"] = float(duration_full[0][0])
+                        self.get_period(duration_full[0][1].lower(), course_item)
 
-        if 'fees' in course and '2021' in course['fees']:
-            if 'domesticTuitionMaxFee' in course['fees']['2021'] and \
-                    course['fees']['2021']['domesticTuitionMaxFee']:
-                course_item['domesticFeeAnnual'] = course['fees']['2021']['domesticTuitionMaxFee']
-                get_total("domesticFeeAnnual", "domesticFeeTotal", course_item)
+            if 'fees' in course and '2021' in course['fees']:
+                if 'domesticTuitionMaxFee' in course['fees']['2021'] and \
+                        course['fees']['2021']['domesticTuitionMaxFee']:
+                    course_item['domesticFeeAnnual'] = course['fees']['2021']['domesticTuitionMaxFee']
+                    get_total("domesticFeeAnnual", "domesticFeeTotal", course_item)
 
-        if 'fees' in course and '2021' in course['fees']:
-            if 'internationalTuitionFee' in course['fees']['2021'] and \
-                    course['fees']['2021']['internationalTuitionFee']:
-                course_item['internationalFeeAnnual'] = course['fees']['2021']['internationalTuitionFee']
-                get_total("internationalFeeAnnual", "internationalFeeTotal", course_item)
+            if 'fees' in course and '2021' in course['fees']:
+                if 'internationalTuitionFee' in course['fees']['2021'] and \
+                        course['fees']['2021']['internationalTuitionFee']:
+                    course_item['internationalFeeAnnual'] = course['fees']['2021']['internationalTuitionFee']
+                    get_total("internationalFeeAnnual", "internationalFeeTotal", course_item)
 
-        course_item.set_sf_dt(self.degrees, degree_delims=["and", "/"], type_delims=["of", "in", "by"])
+            course_item.set_sf_dt(self.degrees, degree_delims=["and", "/"], type_delims=["of", "in", "by"])
 
-        if 'teachingPeriod' in course_item and 'durationMinFull' in course_item and \
-                ((course_item['teachingPeriod'] == 1 and course_item['durationMinFull'] <= 0.5) or
-                 (course_item['teachingPeriod'] == 12 and course_item['durationMinFull'] <= 6)):
-            course_item['degreeType'] = 'Short course or microcredential'
+            if 'teachingPeriod' in course_item and 'durationMinFull' in course_item and \
+                    ((course_item['teachingPeriod'] == 1 and course_item['durationMinFull'] <= 0.5) or
+                     (course_item['teachingPeriod'] == 12 and course_item['durationMinFull'] <= 6)):
+                course_item['degreeType'] = 'Short course or microcredential'
 
-        course_item['group'] = 2
-        course_item['canonicalGroup'] = 'GradNewZealand'
+            course_item['group'] = 2
+            course_item['canonicalGroup'] = 'GradNewZealand'
 
-        if re.search('computer networking', name, flags=re.I) and \
-                re.search('certificate', name, flags=re.I):
-            course_item['degreeType'] = 'Certificate'
+            if re.search('computer networking', name, flags=re.I) and \
+                    re.search('certificate', name, flags=re.I):
+                course_item['degreeType'] = 'Certificate'
 
-        if name:
-            course_item.set_course_name(name.strip(), self.uidPrefix)
+            if name:
+                course_item.set_course_name(name.strip(), self.uidPrefix)
 
-        if 'uid' in course_item and 'courseCode' in course_item:
-            course_item['uid'] += '-' + course_item['courseCode']
+            if 'uid' in course_item and 'courseCode' in course_item:
+                course_item['uid'] += '-' + course_item['courseCode']
 
-        yield course_item
+            yield course_item
