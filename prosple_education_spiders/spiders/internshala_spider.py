59741bf7811f9c1837d982d0e36928f72aaee389
ChampionChan000 <christian.anasco@prosple.com>
2021-06-03 01:00:34 +0800

    new spiders: hkgc_spider.py, internshala_spider.py

---
+++ prosple_education_spiders/spiders/internshala_spider.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# by Christian Anasco
+
+from ..standard_libs import *
+from ..scratch_file import strip_tags
+
+
+class Opportunity(scrapy.Item):
+    opportunity_name = scrapy.Field()
+
+
+class InternshalaSpiderSpider(scrapy.Spider):
+    name = 'internshala_spider'
+    start_urls = ['https://internshala.com/internships/page-1']
+
+    def parse(self, response):
+        total_jobs = response.xpath(
+            "//div[@id='internship_seo_heading_container']/div[contains(@class, 'heading')]").get()
+        if total_jobs:
+            total_jobs = int(re.findall('(\d+) total internship', total_jobs)[0])
+            if total_jobs:
+                for num in range(1, total_jobs + 1):
+                    url = f'https://internshala.com/internships/page-{num}'
+                    yield response.follow(url, callback=self.job_parse)
+
+    def job_parse(self, response):
+        job_item = Opportunity()
+
+        job_name = response.xpath("//div[contains(@class, 'heading_title')]/text()").get()
+        if job_name:
+            job_item['opportunity_name'] = job_name.strip()
+
+        yield job_name
