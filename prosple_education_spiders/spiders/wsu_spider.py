e58860a7a79ea485641ae5bdcb48328de34a6efc
ChampionChan000 <christian.anasco@prosple.com>
2021-06-17 19:29:14 +0800

    updated wsu_spider.py

--- prosple_education_spiders/spiders/wsu_spider.py
+++ prosple_education_spiders/spiders/wsu_spider.py
@@ -245,7 +245,12 @@ class WsuSpiderSpider(scrapy.Spider):
         if study_holder:
             course_item["modeOfStudy"] = "|".join(study_holder)
 
-        course_item.set_sf_dt(self.degrees, degree_delims=["and", "/"], type_delims=["of", "in", "by"])
+        if course_item['courseName'] in ['Bachelor of Planning (Pathway to Master of Urban Management and Planning)']:
+            course_item['courseName'] = 'Bachelor of Planning'
+            course_item.set_sf_dt(self.degrees, degree_delims=["and", "/"], type_delims=["of", "in", "by"])
+            course_item['courseName'] = 'Bachelor of Planning (Pathway to Master of Urban Management and Planning)'
+        else:
+            course_item.set_sf_dt(self.degrees, degree_delims=["and", "/"], type_delims=["of", "in", "by"])
 
         if course_item['sourceURL'] not in self.banned_urls:
             yield course_item
