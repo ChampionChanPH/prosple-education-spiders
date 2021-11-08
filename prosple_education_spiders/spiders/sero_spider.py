949dd302f652ccf0f392062ab0606f0bbab32ecb
Chan <christian.anasco@gmail.com>
2021-11-08 18:57:21 +0800

    added new spider, sero_spider.py

---
+++ prosple_education_spiders/spiders/sero_spider.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+# by Christian Anasco
+
+from ..standard_libs import *
+from ..scratch_file import strip_tags
+
+
+def research_coursework(course_item):
+    if re.search("research", course_item["courseName"], re.I):
+        return "12"
+    else:
+        return "11"
+
+
+def bachelor_honours(course_item):
+    if re.search("honours", course_item["courseName"], re.I):
+        return "3"
+    else:
+        return "2"
+
+
+def get_total(field_to_use, field_to_update, course_item):
+    if "durationMinFull" in course_item and "teachingPeriod" in course_item:
+        if course_item["teachingPeriod"] == 1:
+            if float(course_item["durationMinFull"]) < 1:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"])
+        if course_item["teachingPeriod"] == 12:
+            if float(course_item["durationMinFull"]) < 12:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                                               / 12
+        if course_item["teachingPeriod"] == 52:
+            if float(course_item["durationMinFull"]) < 52:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                                               / 52
+
+
+class SeroSpiderSpider(scrapy.Spider):
+    name = 'sero_spider'
+    start_urls = ['https://seroinstitute.com.au/']
+    institution = "Navitas English"
+    uidPrefix = "AU-NAVE-"
+
+    degrees = {
+        "graduate certificate": "7",
+        "online graduate certificate": "7",
+        "graduate diploma": "8",
+        "online graduate diploma": "8",
+        "master": research_coursework,
+        "executive master": research_coursework,
+        "online master": research_coursework,
+        "bachelor": bachelor_honours,
+        "doctor": "6",
+        "phd": "6",
+        "certificate": "4",
+        "vce": "4",
+        "undergraduate certificate": "4",
+        "certificate i": "4",
+        "certificate ii": "4",
+        "certificate iii": "4",
+        "certificate iv": "4",
+        "advanced diploma": "5",
+        "diploma": "5",
+        "associate degree": "1",
+        "non-award": "13",
+        "no match": "15"
+    }
+
+    months = {
+        "January": "01",
+        "February": "02",
+        "March": "03",
+        "April": "04",
+        "May": "05",
+        "June": "06",
+        "July": "07",
+        "August": "08",
+        "September": "09",
+        "October": "10",
+        "November": "11",
+        "December": "12"
+    }
+
+    campuses = {
+        "North Metropolitan TAFE Perth": "30917",
+        "Sydney Hyde Park": "30919",
+        "Perth": "30918",
+        "Manly Beach": "30916",
+        "Darwin": "30915",
+        "Brisbane": "30914",
+    }
+
+    teaching_periods = {
+        "year": 1,
+        "semester": 2,
+        "trimester": 3,
+        "quarter": 4,
+        "month": 12,
+        "week": 52,
+        "day": 365
+    }
+
+    def get_period(self, string_to_use, course_item):
+        for item in self.teaching_periods:
+            if re.search(item, string_to_use):
+                course_item["teachingPeriod"] = self.teaching_periods[item]
+
+    def parse(self, response):
+        sub = response.xpath("//a[@class='menu-link' and text()='Courses']/following-sibling::ul["
+                             "@class='sub-menu']//ul[@class='sub-menu']//a")
+        yield from response.follow_all(sub, callback=self.sub_parse)
+
+    def sub_parse(self, response):
+        courses = response.xpath("//a[span/span/text()='Course Info']")
+        yield from response.follow_all(courses, callback=self.sub_parse)
+
+    def course_parse(self, response):
+        course_item = Course()
+
+        course_item['lastUpdate'] = date.today().strftime("%m/%d/%y")
+        course_item['sourceURL'] = response.request.url
+        course_item['published'] = 1
+        course_item['institution'] = self.institution
+        course_item["domesticApplyURL"] = response.request.url
+
+        name = response.xpath("//h1[contains(@class, 'elementor-heading-title')]/text()").get()
+        if name:
+            if re.search("[A-Z]+[0-9]+ ", name):
+                course_code, course_name = re.split("\\s", name, maxsplit=1)
+                course_item.set_course_name(course_name.strip(), self.uidPrefix)
+                course_item["courseCode"] = course_code[0]
+            else:
+                course_item.set_course_name(name.strip(), self.uidPrefix)
+
+        overview = response.xpath(
+            "//*[@data-element_type='widget' and */h1[contains(@class, 'elementor-heading-title')]]"
+            "/following-sibling::*//*[self::p or self::ol or self::ul]").getall()
+        if overview:
+            summary = [strip_tags(x) for x in overview]
+            course_item.set_summary(' '.join(summary))
+            course_item["overview"] = strip_tags(''.join(overview), remove_all_tags=False, remove_hyperlinks=True)
+
+        entry = response.xpath(
+            "//*[@data-element_type='widget' and */h2/text()='Entry Requirements']"
+            "/following-sibling::*//*[self::p or self::ol or self::ul]").getall()
+        if entry:
+            course_item["entryRequirements"] = strip_tags(''.join(entry), remove_all_tags=False, remove_hyperlinks=True)
+
+        duration = response.xpath(
+            "//*[@data-element_type='widget' and (contains(*/h2/text(), 'Course duration') or "
+            "contains(*/h2/text(), 'Course Duration'))]/following-sibling::*//*[self::p or self::ol or "
+            "self::ul]").getall()
+        if duration:
+            duration_full = re.findall(
+                "(\d*\.?\d+)(?=\s(year|month|semester|trimester|quarter|week|day)s?\s+?full)",
+                duration, re.I | re.M | re.DOTALL)
+            duration_part = re.findall(
+                "(\d*\.?\d+)(?=\s(year|month|semester|trimester|quarter|week|day)s?\s+?part)",
+                duration, re.I | re.M | re.DOTALL)
+            if not duration_full and duration_part:
+                self.get_period(duration_part[0][1].lower(), course_item)
+            if duration_full:
+                course_item["durationMinFull"] = float(duration_full[0][0])
+                self.get_period(duration_full[0][1].lower(), course_item)
+            if duration_part:
+                if self.teaching_periods[duration_part[0][1].lower()] == course_item["teachingPeriod"]:
+                    course_item["durationMinPart"] = float(duration_part[0][0])
+                else:
+                    course_item["durationMinPart"] = float(duration_part[0][0]) * course_item["teachingPeriod"] \
+                                                     / self.teaching_periods[duration_part[0][1].lower()]
+            if "durationMinFull" not in course_item and "durationMinPart" not in course_item:
+                duration_full = re.findall("(\d*\.?\d+)(?=\s(year|month|semester|trimester|quarter|week|day))",
+                                           duration, re.I | re.M | re.DOTALL)
+                if duration_full:
+                    course_item["durationMinFull"] = float(duration_full[0][0])
+                    self.get_period(duration_full[0][1].lower(), course_item)
+                    # if len(duration_full) == 1:
+                    #     course_item["durationMinFull"] = float(duration_full[0][0])
+                    #     self.get_period(duration_full[0][1].lower(), course_item)
+                    # if len(duration_full) == 2:
+                    #     course_item["durationMinFull"] = min(float(duration_full[0][0]), float(duration_full[1][0]))
+                    #     course_item["durationMaxFull"] = max(float(duration_full[0][0]), float(duration_full[1][0]))
+                    #     self.get_period(duration_full[1][1].lower(), course_item)
+
+        yield course_item
