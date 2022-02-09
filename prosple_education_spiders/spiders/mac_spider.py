87b0963961911a2ed79f6ce1f6098c3fd7adb0e7
Chan <christian.anasco@gmail.com>
2022-02-09 11:57:44 +0800

    added spiders

--- prosple_education_spiders/spiders/mac_spider.py
+++ prosple_education_spiders/spiders/mac_spider.py
@@ -1,20 +1,134 @@
 # -*- coding: utf-8 -*-
 # by Christian Anasco
-# having difficulties getting the courses
+# issues with the scraper
 
 from ..standard_libs import *
-from ..scratch_file import strip_tags
+from ..scratch_file import *
+
+
+def research_coursework(course_item):
+    if re.search("research", course_item["courseName"], re.I):
+        return "12"
+    else:
+        return "11"
+
+
+def bachelor_honours(course_item):
+    if re.search("honours", course_item["courseName"], re.I):
+        return "3"
+    else:
+        return "2"
+
+
+def get_total(field_to_use, field_to_update, course_item):
+    if "durationMinFull" in course_item and "teachingPeriod" in course_item:
+        if course_item["teachingPeriod"] == 1:
+            if float(course_item["durationMinFull"]) < 1:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"])
+        if course_item["teachingPeriod"] == 12:
+            if float(course_item["durationMinFull"]) < 12:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                                               / 12
+        if course_item["teachingPeriod"] == 52:
+            if float(course_item["durationMinFull"]) < 52:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                                               / 52
 
 
 class MacSpiderSpider(scrapy.Spider):
     name = 'mac_spider'
     allowed_domains = ['courses.mq.edu.au', 'mq.edu.au']
-    start_urls = ['https://courses.mq.edu.au/search?query=&refinementList%5Byear%5D%5B0%5D=2020&refinementList'
-                  '%5Bdomestic%5D%5B0%5D=true&configure%5BhitsPerPage%5D=1000&configure%5BmaxValuesPerFacet%5D=100']
+    start_urls = ['https://www.mq.edu.au/study/find-a-course/courses/bachelor-of-speech-and-hearing-sciences']
     http_user = 'b4a56de85d954e9b924ec0e0b7696641'
+    institution = "Macquarie University"
+    uidPrefix = "AU-MAC-"
+
+    degrees = {
+        "graduate certificate": "7",
+        "graduate diploma": "8",
+        "master": research_coursework,
+        "bachelor": bachelor_honours,
+        "doctor": "6",
+        "certificate": "4",
+        "certificate i": "4",
+        "certificate ii": "4",
+        "certificate iii": "4",
+        "certificate iv": "4",
+        "advanced diploma": "5",
+        "diploma": "5",
+        "associate degree": "1",
+        "vcal in victorian certificate": "9",
+        "vcal in": "9",
+        "non-award": "13",
+        "no match": "15"
+    }
+
+    months = {
+        "January": "01",
+        "February": "02",
+        "March": "03",
+        "April": "04",
+        "May": "05",
+        "June": "06",
+        "July": "07",
+        "August": "08",
+        "September": "09",
+        "October": "10",
+        "November": "11",
+        "December": "12"
+    }
+
+    campuses = {
+        "Heidelberg": "57293",
+        "Epping": "57292",
+        "Fairfield": "57291",
+        "Preston": "57290",
+        "Greensborough": "57294",
+        "Prahran": "57295",
+        "Collingwood": "57296",
+        "Eden Park at Northern Lodge": "57297",
+        "Yan Yean at Northern Lodge": "57299",
+        "Online": "57300",
+    }
+
+    teaching_periods = {
+        "year": 1,
+        "semester": 2,
+        "trimester": 3,
+        "quarter": 4,
+        "month": 12,
+        "week": 52,
+        "day": 365
+    }
+
+    def get_period(self, string_to_use, course_item):
+        for item in self.teaching_periods:
+            if re.search(item, string_to_use):
+                course_item["teachingPeriod"] = self.teaching_periods[item]
 
     def parse(self, response):
-        yield SplashRequest(response.request.url, callback=self.sub_parse, args={'wait': 20})
+        course_item = Course()
+
+        course_item['lastUpdate'] = date.today().strftime("%m/%d/%y")
+        course_item['sourceURL'] = response.request.url
+        course_item['published'] = 1
+        course_item['institution'] = self.institution
+
+        course_name = response.xpath("//h1/text()").get()
+        if course_name:
+            course_item.set_course_name(strip_tags(course_name.strip()), self.uidPrefix)
+
+
+
+        course_item.set_sf_dt(self.degrees, degree_delims=["and", "/"], type_delims=["of", "in", "by"])
+
+        yield course_item
 
     def sub_parse(self, response):
         title = response.xpath("//title/text()").get()
