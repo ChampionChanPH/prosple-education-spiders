bf788b09931587b274084f1691371075a4d194e1
Chan <christian.anasco@gmail.com>
2022-02-21 14:07:26 +0800

    updated spider, anu_spider.py - updated lua script to click all `show all results` buttons

--- prosple_education_spiders/spiders/anu_spider.py
+++ prosple_education_spiders/spiders/anu_spider.py
@@ -66,7 +66,7 @@ class AnuSpiderSpider(scrapy.Spider):
       a = 1
       while(a < 5)
       do
-        a = a + 1
+        a = a+1
         local element = splash:select('a[data-template="program-template"]')
         assert(element:mouse_click())
         assert(splash:wait(5))
