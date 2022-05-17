5b0783e695a49cbb1479bb35ebc58d6db904b77a
Chan <christian.anasco@gmail.com>
2022-05-17 15:55:47 +0800

    updated csf_spider

--- prosple_education_spiders/spiders/csf_spider.py
+++ prosple_education_spiders/spiders/csf_spider.py
@@ -1,10 +1,118 @@
-import scrapy
+# -*- coding: utf-8 -*-
+# by Christian Anasco
+
+from ..standard_libs import *
+from ..scratch_file import strip_tags
+
+
+def research_coursework(course_item):
+    if re.search("research", course_item["courseName"], re.I):
+        return "12"
+    else:
+        return "11"
+
+
+def bachelor_honours(course_item):
+    if re.search("honours", course_item["courseName"], re.I):
+        return "3"
+    else:
+        return "2"
+
+
+def get_total(field_to_use, field_to_update, course_item):
+    if "durationMinFull" in course_item and "teachingPeriod" in course_item:
+        if course_item["teachingPeriod"] == 1:
+            if float(course_item["durationMinFull"]) < 1:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(
+                    course_item[field_to_use]) * float(course_item["durationMinFull"])
+        if course_item["teachingPeriod"] == 12:
+            if float(course_item["durationMinFull"]) < 12:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                    / 12
+        if course_item["teachingPeriod"] == 52:
+            if float(course_item["durationMinFull"]) < 52:
+                course_item[field_to_update] = course_item[field_to_use]
+            else:
+                course_item[field_to_update] = float(course_item[field_to_use]) * float(course_item["durationMinFull"]) \
+                    / 52
 
 
 class CsfSpiderSpider(scrapy.Spider):
     name = 'csf_spider'
-    allowed_domains = ['a']
-    start_urls = ['http://a/']
+    start_urls = ['https://wbt3yd48-staging.dreamwp.com/courses/']
+    institution = "University of Canberra"
+    uidPrefix = "AU-UOC-"
+    http_user = 'b4a56de85d954e9b924ec0e0b7696641'
+
+    degrees = {
+        "graduate certificate": "7",
+        "graduate diploma": "8",
+        "master": research_coursework,
+        # One course "Honours in Information Sciences" not captured, manually
+        "bachelor": bachelor_honours,
+        # updated
+        "doctor": "6",
+        "professional doctorate": "6",
+        "certificate": "4",
+        "undergraduate certificate": "4",
+        "certificate i": "4",
+        "certificate ii": "4",
+        "certificate iii": "4",
+        "certificate iv": "4",
+        "advanced diploma": "5",
+        "diploma": "5",
+        "associate degree": "1",
+        "university foundation studies": "13",
+        # "University of Canberra International Foundation Studies" not captured, manually
+        "non-award": "13",
+        # updated
+        "no match": "15"
+    }
+
+    months = {
+        "January": "01",
+        "February": "02",
+        "March": "03",
+        "April": "04",
+        "May": "05",
+        "June": "06",
+        "July": "07",
+        "August": "08",
+        "September": "09",
+        "October": "10",
+        "November": "11",
+        "December": "12"
+    }
+
+    campuses = {
+        "Singapore": "738",
+        "Canberra": "735",
+        "Sydney": "737"
+    }
+
+    teaching_periods = {
+        "year": 1,
+        "semester": 2,
+        "trimester": 3,
+        "quarter": 4,
+        "month": 12,
+        "week": 52,
+        "day": 365
+    }
+
+    def get_period(self, string_to_use, course_item):
+        for item in self.teaching_periods:
+            if re.search(item, string_to_use):
+                course_item["teachingPeriod"] = self.teaching_periods[item]
 
     def parse(self, response):
-        pass
+        yield SplashRequest(response.request.url, callback=self.splash_parse, args={'wait': 20})
+
+    def splash_parse(self, response):
+        courses = response.xpath("//a[@class='pushed']/@href").getall()
+
+        print(courses)
